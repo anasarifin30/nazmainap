7ef98bc623a6e1d7b2d1008b52a4e95005317266
anasarifin30 <anasarifin30@students.amikom.ac.id>
2025-04-25 15:51:06 +0700

    relasi antar tabel

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -7,6 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Room extends Model
 {
+    public function homestay()
+    {
+        return $this->belongsTo(Homestay::class);
+    }
+
+    public function photos()
+    {
+        return $this->hasMany(RoomPhoto::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function facilities()
+    {
+        return $this->belongsToMany(Facility::class, 'room_facility');
+    }
+
     /** @use HasFactory<\Database\Factories\RoomFactory> */
     use HasFactory;
 }
