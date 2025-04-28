bb1617cc422957630aabe4f7e4518ef785d51422
anasarifin30 <anasarifin30@students.amikom.ac.id>
2025-04-28 15:48:09 +0700

    update migrate

---
+++ database/migrations/2025_04_31_082224_create_booking_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('booking_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('booking_id')
+                  ->constrained('bookings')
+                  ->onDelete('cascade');
+            $table->foreignId('room_id')
+                  ->constrained('rooms')
+                  ->onDelete('cascade');
+            $table->unsignedInteger('quantity');      // jumlah kamar yang dibooking
+            $table->decimal('price_per_night', 10, 2); // harga per malam per kamar
+            $table->decimal('subtotal_price', 10, 2);  // harga kamar x quantity x durasi
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('booking_details');
+    }
+};
