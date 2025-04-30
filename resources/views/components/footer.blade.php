eab8bff4711c5ca347b86b8e887c8baa4b753173
labibahraihanatul <miwalare@gmail.com>
2025-04-30 11:39:25 +0700

    rename folder

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,24 @@
+<footer style="background-color: #312e81; color: white;" class="py-4 mt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 mb-4 mb-md-0">
+          <h5 class="mb-3">Punya rumah kosong atau kamar tersedia?</h5>
+          <a href="/daftarkan-sekarang" class="btn rounded-pill px-4 py-2" style="background-color: #f59e0b; color: white;">Daftarkan sekarang</a>
+        </div>
+        <div class="col-md-6 text-md-end">
+          <h5 class="mb-3">Kontak Kami</h5>
+          <div class="d-flex justify-content-md-end">
+            <a href="mailto:info@nazmainap.com" class="text-white me-3"><i class="fas fa-envelope"></i></a>
+            <a href="https://instagram.com/nazmainap" class="text-white me-3"><i class="fab fa-instagram"></i></a>
+            <a href="https://facebook.com/nazmainap" class="text-white me-3"><i class="fab fa-facebook"></i></a>
+            <a href="https://linkedin.com/company/nazmainap" class="text-white me-3"><i class="fab fa-linkedin"></i></a>
+            <a href="https://tiktok.com/@nazmainap" class="text-white"><i class="fab fa-tiktok"></i></a>
+          </div>
+        </div>
+      </div>
+      <hr class="my-4">
+      <div class="text-center">
+        <p class="mb-0">Copyright 2025 - Develop by <a href="#" class="text-white text-decoration-none">Nazbla Office</a></p>
+      </div>
+    </div>
+  </footer>
